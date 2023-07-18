421f4f7b7fcf455c1a992da0e6814c856c682a1c
skorodenko <72010957+skorodenko@users.noreply.github.com>
2023-07-18 15:16:42 +0300

    Final fix

--- app/widgets/main.py
+++ app/widgets/main.py
@@ -32,38 +32,32 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.tableView.setModel(self.table_model)
 
         self.actionSave_as.triggered.connect(self.saveAs)
-        self.actionAdd_One_2.triggered.connect(self.addRow)
         self.actionAdd_One.triggered.connect(self.addColumn)
+        self.actionAdd_One_2.triggered.connect(self.addRow)
 
         self.show()
 
     def add_row(self, n=1):
-        if n == 0:
-            self.table_model.update_table(self.table)
-            return None
-        new_col = np.full((1, self.table.shape[1]), "")
+        new_col = np.full((n, self.table.shape[1]), "")
         self.table = np.vstack([self.table, new_col])
-        self.add_column(n=n - 1)
+        self.table_model.update_table(self.table)
 
     def add_column(self, n=1):
-        if n == 0:
-            self.table_model.update_table(self.table)
-            return None
-        new_row = np.full((self.table.shape[0], 1), "")
+        new_row = np.full((self.table.shape[0], n), "")
         self.table = np.hstack([self.table, new_row])
-        self.add_row(n=n - 1)
-
-    def addColumn(self):
-        dlg = DialogExpandTable("Add columns", parent=self)
-        if dlg.exec():
-            n = dlg.spinBox.value()
-            self.add_column(n)
+        self.table_model.update_table(self.table)
 
     def addRow(self):
         dlg = DialogExpandTable("Add rows", parent=self)
         if dlg.exec():
             n = dlg.spinBox.value()
             self.add_row(n)
+
+    def addColumn(self):
+        dlg = DialogExpandTable("Add columns", parent=self)
+        if dlg.exec():
+            n = dlg.spinBox.value()
+            self.add_column(n)
 
     def save_to_docx(self, filepath):
         table = self.table
